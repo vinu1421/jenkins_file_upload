c059395eac80fa865cfa2f3b5ee02d5d6c858708
vinayak <vinayak.prasannakumar@mindcurv.com>
2021-11-18 20:14:04 +0530

    creating new jenkinsfile

---
+++ jenkinsfile.groovy
@@ -0,0 +1,22 @@
+node('master') {
+timestamps {
+
+	deleteDir()
+
+	stage('Request files'){
+
+
+		def userInput
+		timeout(time: 5, unit: 'MINUTES') {
+			// userInput = input(id: 'userInput', message: 'Files to import', parameters: parameters)
+            userInput = input message: 'Input the file ', parameters: [file(description: '', name: 'vinu')]
+		}
+
+	    sh 'mv vinu ${WORKSPACE}/'
+			
+
+			
+		
+	}
+}
+}
