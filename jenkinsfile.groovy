008581348ad792d0921a9870ebee554e48bb7d48
vinayak <vinayak.prasannakumar@mindcurv.com>
2021-11-18 20:31:35 +0530

    testing

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,14 +5,8 @@ node('master') {
 
 	stage('Request files'){
 
-
-		def userInput
-		timeout(time: 5, unit: 'MINUTES') {
-			// userInput = input(id: 'userInput', message: 'Files to import', parameters: parameters)
-            userInput = input message: 'Input the file ', parameters: [file(description: '', name: 'vinu')]
-		}
-
-	    sh 'mv ' + userInput['vinu'] + ' ${WORKSPACE}/'
+       userInput = input message: 'Input the file ', parameters: [file(description: '', name: 'vinu')]
+	   sh 'mv $vinu ${WORKSPACE}/'
 			
 
 			
