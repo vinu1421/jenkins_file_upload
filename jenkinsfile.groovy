c9ade07c972bf2a626c929c6bd8338493a104ccf
vinayak <vinayak.prasannakumar@mindcurv.com>
2021-11-18 20:51:45 +0530

    testing with something

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,8 +6,9 @@ node('master') {
 	stage('Request files'){
 
     //    userInput = input message: 'Input the file ', parameters: [file(description: '', name: 'vinu')]
-      properties([[$class: 'JiraProjectProperty'], parameters([file(description: '', name: 'vinu')])])
-	  sh 'ls -l'
+      properties([[$class: 'JiraProjectProperty'], parameters([file(description: '', name: 'file')])])
+      def file_in_workspace = unstashParam "file"
+	  sh "cat ${file_in_workspace}"
 			
 
 			
